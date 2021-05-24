c040466c7dc0c50cb2c97d2ee3cecdc25189f43a
Ilya <ilshishkin@gmail.com>
2021-05-25 00:16:12 +0300

    rename project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/tronline-lk-front'),
+      dir: require('path').join(__dirname, './coverage/lk-front'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tronline-lk-front'`, () => {
+  it(`should have as title 'lk-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tronline-lk-front');
+    expect(app.title).toEqual('lk-front');
   });
 
   it('should render title', () => {
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain(
-      'tronline-lk-front app is running!',
+      'lk-front app is running!',
     );
   });
 });
